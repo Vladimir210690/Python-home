d2a50a355b0cf6a6a46b7db559274a0436223677
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-18 21:36:21 +0300

    Проба вывести рамку

---
+++ рамка.py
@@ -0,0 +1,17 @@
+col = 20
+row = 20
+i = 0
+j = 0
+while i < col:
+    print("#", end = '')
+    i += 1
+if i == 20:
+    i = 0
+print()
+while i < col:
+    print("#", end = '')
+    i += 1
+if i == 20:
+    i = 0
+print()
+
